032255a78a034d46413f66f5ab97e6025fa3dd5c
taleenteen <teenteenza1@hotmail.com>
2024-08-01 13:14:40 +0700

    qc

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -9,12 +9,10 @@ const toggleMenu = () => {
 </script>
 
 <template>
-  <nav
-    class="bg-gray-700 w-full z-20 top-0 start-0 border-b border-gray-200 border-gray-600"
-  >
+  <nav class="bg-gray-700 w-full z-20 top-0 start-0 border-b border-gray-600">
     <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
       <RouterLink
-        class="block py-2 px-3 rounded hover:text-blue-700 m md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:hover:text-blue-500 text-white"
+        class="block py-2 px-3 rounded hover:text-blue-700 m md:hover:bg-transparent md:p-0 md:hover:text-blue-500 text-white"
         to="/"
         ><li class="pi pi-home text-white mx-5"></li>
         <span class="self-center text-2xl font-semibold whitespace-nowrap"
@@ -36,7 +34,7 @@ const toggleMenu = () => {
         id="navbar-sticky"
       >
         <ul
-          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-700 border-gray-700"
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border rounded-lg md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-700 border-gray-700"
         >
           <li>
             <RouterLink
