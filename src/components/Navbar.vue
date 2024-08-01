8db83c31e6dee11259a6e0d7440de58102bccab2
taleenteen <teenteenza1@hotmail.com>
2024-08-01 11:58:15 +0700

    fix navbar color bug

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,7 +14,7 @@ const toggleMenu = () => {
   >
     <div class="max-w-screen-xl flex items-center justify-between mx-auto p-4">
       <RouterLink
-        class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:hover:text-blue-500 text-white"
+        class="block py-2 px-3 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:hover:text-blue-500 text-white"
         to="/"
         ><li class="pi pi-home text-white mx-5"></li>
         <span class="self-center text-2xl font-semibold whitespace-nowrap"
@@ -36,7 +36,7 @@ const toggleMenu = () => {
         id="navbar-sticky"
       >
         <ul
-          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white bg-gray-800 md:bg-gray-700 border-gray-700"
+          class="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 bg-gray-800 md:bg-gray-700 border-gray-700"
         >
           <li>
             <RouterLink
